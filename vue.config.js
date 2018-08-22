9e6693d3a97ac8b2fd3b0121f37106d5fa8c1fa0
Zou Jian <chsword@126.com>
2018-08-23 01:52:34 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  baseUrl: './'
+  baseUrl: process.env.NODE_ENV=='development'?'/': './',
+
+  devServer: {
+  },
+
+  outputDir: undefined,
+  assetsDir: 'lib',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
